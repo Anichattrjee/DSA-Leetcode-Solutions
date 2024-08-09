fd5863539e2db2d4f4a422361cee20d7324eb995
Aniruddha Chatterjee <110561980+Anichattrjee@users.noreply.github.com>
2024-08-09 23:48:51 +0530

    Time: 16 ms (70.47%), Space: 22.3 MB (27.13%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,45 +1,31 @@
-
 class MinStack {
 public:
-    std::stack<long long> st;
-    long long min = LLONG_MAX; // Use LLONG_MAX for initialization
-
+    stack<pair<int,int>>st;
     MinStack() {
+        
     }
     
     void push(int val) {
-        long long value = static_cast<long long>(val); // Cast val to long long
-        if (st.empty()) {
-            st.push(value);
-            min = value;
-        } else {
-            if (value < min) {
-                st.push(2 * value - min); // Ensure this calculation uses long long
-                min = value;
-            } else {
-                st.push(value);
-            }
+        if(st.empty())
+        {
+            st.push({val,val});
+        }
+        else
+        {
+            st.push({val,min(val,st.top().second)});
         }
     }
     
     void pop() {
-        if (st.empty()) return;
-        long long x = st.top();
         st.pop();
-        if (x < min) {
-            min = 2 * min - x; // Ensure this calculation uses long long
-        }
     }
     
     int top() {
-        if (st.empty()) return -1;
-        long long x = st.top();
-        if (x < min) return static_cast<int>(min); // Cast min to int
-        return static_cast<int>(x); // Cast x to int
+        return st.top().first;
     }
     
     int getMin() {
-        return static_cast<int>(min); // Cast min to int
+        return st.top().second;
     }
 };
 
