c3a419e81417e32806e8988b05abfb03e392db32
Aniruddha Chatterjee <110561980+Anichattrjee@users.noreply.github.com>
2024-09-25 10:36:05 +0530

    Time: 8 ms (38.94%), Space: 10.5 MB (51.28%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1)return "1";
+        string s=countAndSay(n-1);
+        string res="";
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s[i];
+            int cnt=1;
+
+            while(i<s.length()-1 && s[i]==s[i+1])
+            {
+                cnt++;
+                i++;
+            }
+            res+=to_string(cnt)+string(1,c);
+        }
+        return res;
+    }
+};
