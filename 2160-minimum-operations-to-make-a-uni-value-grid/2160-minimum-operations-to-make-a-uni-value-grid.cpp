bef5fde23fe6efdcbd4c0ba1fcbe86e55dec8e2f
Aniruddha Chatterjee <110561980+Anichattrjee@users.noreply.github.com>
2025-03-26 22:15:20 +0530

    Time: 45 ms (48.94%), Space: 90.1 MB (34.95%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -4,7 +4,7 @@ public:
         vector<int>a;
         int n=grid.size();
         int m=grid[0].size();
-
+        //convert the grid into an array
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
@@ -12,11 +12,14 @@ public:
                 a.push_back(grid[i][j]);
             }
         }
+        //sort the array and find the median to get the target element
         sort(a.begin(),a.end());
 
         int l=a.size();
+        //find the target element 
         int target=a[l/2];
 
+        //check if (target-a)%x==0 true for all elements or not otherwise return 1(then its impossible to convert)
         for(int i=0;i<l;i++)
         {
             if((target-a[i])%x!=0)
@@ -24,7 +27,7 @@ public:
                 return -1;
             }
         }
-
+        //we know noOfOperations=|(target-a)|/x;
         int count=0;
         for(int i=0;i<l;i++)
         {
